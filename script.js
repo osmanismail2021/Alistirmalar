62297b1ff2fa9940cc77da3b698d79d53160ed26
osmanismail2021 <93704746+osmanismail2021@users.noreply.github.com>
2022-02-10 22:38:14 +0100

    update

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
-var sahis = prompt("Lutfen Yasinizi Giriniz") ;
+// var sahis = prompt("Lutfen Yasinizi Giriniz") ;
 
-if (sahis < 18){
-alert("Kafeye Giremez");
-}else if(sahis > 18 && sahis < 23){
-alert("Kafeye Girebilirsin ama Alkol alamazsin");
-}else {
-alert("Kafeye girebilir ve Alkol alabilirsin");
+// if (sahis < 18){
+// alert("Kafeye Giremez");
+// }else if(sahis > 18 && sahis < 23){
+// alert("Kafeye Girebilirsin ama Alkol alamazsin");
+// }else {
+// alert("Kafeye girebilir ve Alkol alabilirsin");
+// }
+
+let buton = document.getElementById("tikla")
+buton.addEventListener("click", function() {
+    alert(tikladim)
+})
+,
+
+
+function topla(a, b){
+    /* Gızeml² seyler */
+   return x;
 }
+
+
+function uyari(text){
+    alert(text);
+}
+
+let a,b = 5;
+let x = topla(a,b)
+
+²f(x >10){
+
+}
+
+for(let j=0; j<x; j++){
+    document.createElement("d²v");
+}
+
+let c,d = 10;
+let y = topla(c, 60)
+
+for(k=0; k<y; k++){
+    alert("fddsf");
+}
+
+
+uyari("hata var");
